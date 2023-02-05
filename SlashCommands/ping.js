1abb343a86214b230cede43241fb616f080f3f83
Dev. Jorge Lucas <121786722+trashgeorge@users.noreply.github.com>
2023-02-05 17:10:04 -0300

    bot discord
    (em construção)

---
+++ SlashCommands/ping.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const { EmbedBuilder } = require('discord.js');
+
+module.exports =  {
+    name: "ping",
+    description: "Veja meu ping.",
+    type: 1,    
+    
+    run: async (client, interaction, args) => {
+        let embed = new EmbedBuilder()
+        .setColor("00001")
+        .setDescription(`**\\📡 Meu ping está em** \`${client.ws.ping}ms\`**.**`);
+
+        interaction.reply({ embeds: [embed], ephemeral: true })
+
+    }
+}
